ecad099f6a810b2cfb25a5653f02dc51c1e6ece3
liuxuan.leocoder <liuxuan.leocoder@bytedance.com>
2021-02-02 17:30:50 +0800

    v0.1.3

---
+++ packages/frog-ui/CHANGELOG.md
@@ -0,0 +1,15 @@
+## 0.1.3 (2021-02-02)
+
+
+### Features
+
+* add jest unit test ([16de2ac](https://github.com/leocoder351/frog-kits/commit/16de2acf9380e3923c107d844725bd5a73958495))
+* add lint ([5e056c6](https://github.com/leocoder351/frog-kits/commit/5e056c6a80cb7322cbccb75073a6c2b360b3184a))
+* add lint ([3df729f](https://github.com/leocoder351/frog-kits/commit/3df729f91c0caa67c219e7f50f7d08438ecd00e4))
+* add lint ([fa304df](https://github.com/leocoder351/frog-kits/commit/fa304df5db5a5e6298c3ed5579a3b4fbf984ca43))
+* change js to tsx ([5f76a15](https://github.com/leocoder351/frog-kits/commit/5f76a15c12f06946e93da14f7121a9a1fdb92c8d))
+* 修改 npm 源 ([ddb1fa2](https://github.com/leocoder351/frog-kits/commit/ddb1fa2966c8c930067641865db848ba2c76848c))
+* 修改 README ([cc2e7f5](https://github.com/leocoder351/frog-kits/commit/cc2e7f5477fb6ea75270690818abf41ec461b3f6))
+
+
+
---
+++ packages/frog-ui/build/release.js
@@ -0,0 +1,109 @@
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const util = require('util');
+const semver = require('semver');
+const exec = util.promisify(child_process.exec);
+const semverInc = semver.inc;
+const pkg = require('../package.json');
+
+const currentVersion = pkg.version;
+
+const run = async command => {
+  console.log(chalk.green(command));
+  await exec(command);
+};
+
+const logTime = (logInfo, type) => {
+  const info = `=> ${type}：${logInfo}`;
+  console.log((chalk.blue(`[${new Date().toLocaleString()}] ${info}`)));
+};
+
+const getNextVersions = () => ({
+  major: semverInc(currentVersion, 'major'),
+  minor: semverInc(currentVersion, 'minor'),
+  patch: semverInc(currentVersion, 'patch'),
+  premajor: semverInc(currentVersion, 'premajor'),
+  preminor: semverInc(currentVersion, 'preminor'),
+  prepatch: semverInc(currentVersion, 'prepatch'),
+  prerelease: semverInc(currentVersion, 'prerelease'),
+});
+
+const promptNextVersion = async () => {
+  const nextVersions = getNextVersions();
+  const { nextVersion } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'nextVersion',
+      message: `Please select the next version (current version is ${currentVersion})`,
+      choices: Object.keys(nextVersions).map(name => ({
+        name: `${name} => ${nextVersions[name]}`,
+        value: nextVersions[name]
+      }))
+    }
+  ]);
+  return nextVersion;
+};
+
+const updatePkgVersion = async nextVersion => {
+  pkg.version = nextVersion;
+  logTime('Update package.json version', 'start');
+  await fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(pkg));
+  await run('npx prettier package.json --write');
+  logTime('Update package.json version', 'end');
+};
+
+const genChangelog = async () => {
+  logTime('Generate CHANGELOG.md', 'start');
+  await run(' npx conventional-changelog -p angular -i CHANGELOG.md -s -r 0');
+  logTime('Generate CHANGELOG.md', 'end');
+};
+
+const push = async nextVersion => {
+  logTime('Push Git', 'start');
+  await run('git add .');
+  await run(`git commit -m "v${nextVersion}" -n`);
+  await run('git push');
+  logTime('Push Git', 'end');
+};
+
+const tag = async nextVersion => {
+  logTime('Push Git', 'start');
+  await run(`git tag v${nextVersion}`);
+  await run(`git push origin tag v${nextVersion}`);
+  logTime('Push Git Tag', 'end');
+};
+
+const build = async () => {
+  logTime('Components Build', 'start');
+  await run(`yarn build`);
+  logTime('Components Build', 'end');
+};
+
+const publish = async () => {
+  logTime('Publish Npm', 'start');
+  await run('npm publish');
+  logTime('Publish Npm', 'end');
+};
+
+const main = async () => {
+  try {
+    const nextVersion = await promptNextVersion();
+    const startTime = Date.now();
+
+    await updatePkgVersion(nextVersion);
+    await genChangelog();
+    await push(nextVersion);
+    await build();
+    await publish();
+    await tag(nextVersion);
+
+    console.log(chalk.bgGreen(`Publish Success, Cost ${((Date.now() - startTime) / 1000).toFixed(3)}s`));
+  } catch (err) {
+    console.log(chalk.bgRed(`Publish Fail: ${err}`));
+  }
+}
+
+main()
